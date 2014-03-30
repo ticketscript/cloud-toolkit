b168ace3d2ceae17947b95ea7eb4d1fcdb6c65e6
Geoffrey de Kleijn <jay@jayhaabee.nl>
2014-03-30 15:01:57 +0200

    Added error handling to sync ticketscript2 db script

--- sync-ticketscript2-db.sh
+++ sync-ticketscript2-db.sh
@@ -5,10 +5,20 @@ OFFSET="2000000000"
 USERDATA_SQL_FILE="temp.sql"
 
 # Backup user data in ts2acceptance database first
-./backup-user-data.sh $DATABASE $OFFSET 1> $USERDATA_SQL_FILE
+./backup-user-data.sh $DATABASE $OFFSET 1>$USERDATA_SQL_FILE
+
+if [ "$?" -gt 0 ]; then 
+	echo "ERROR - Backup user data failed!" >&2
+	exit 1
+fi
 
 # Restore acceptance database from snapshot
 ./restore-db-from-slave.sh $INSTANCE
+
+if [ "$?" -gt 0 ]; then 
+	echo "ERROR - Restoring database instance from slave failed!" >&2
+	exit 1
+fi
 
 # Offset AUTOINCREMENT columns
 ./set-autoincrement.sh $DATABASE $OFFSET

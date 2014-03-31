7dc5a67d0ba19e0b8f2391fe6bf83b0f271e0c0d
ticketwww <it@ticketscript.com>
2014-03-31 05:14:02 -0400

    Added DB instance host name in admin stats script

--- admin_stats.sh
+++ admin_stats.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-mysql ts2acceptance < /home/admin/bin/admin_stats.sql
+HOST="ts2acceptance.chw1qgpdiota.eu-west-1.rds.amazonaws.com"
+
+mysql -h $HOST ticketscript2 < /home/admin/bin/admin_stats.sql

688a0e7208b9abb82be228cfde2de7a0204ae932
Martin Hagedoorn <martin@webfish.nl>
2014-05-28 10:45:22 +0200

    TSP-4849: Changed migration execution

---
+++ acceptance-amazon-rds-sync/execute-migration.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Run this after the Acceptance database has been restored from production.
+# This restores any database migration files that were previously run
+#
+
+# Local directory
+DIR=`dirname $0`
+
+source $DIR/config
+
+# Reset 'migration' folder to origin/master
+sudo -u ticketwww sh -c "cd $DATABASE_MIGRATION_SOURCE_DIR && git stash && git fetch -q origin && git checkout -q origin/master"
+
+# Split file names on newline
+IFS=$'\n'
+
+for sql_file_path in `ls -1 $DATABASE_MIGRATION_TARGET_DIR/*.sql`; do
+
+	# Get SQL migration file name
+    sql_file=$(basename $sql_file_path)
+
+	if [ ! -f "$DATABASE_MIGRATION_SOURCE_DIR/$sql_file" ]; then
+		# Execute new MySQL file
+		mysql -h $DATABASE_HOST $DATABASE_NAME < $sql_file_path 1>/dev/null
+
+		# Check for MySQL migration result
+		if [ $? -gt 0 ]; then
+			echo "ERROR - Failed to execute $sql_file!" >&2
+			exit 1
+		fi
+    fi
+done
+
+# Reset Git working folder to previous branch
+sudo -u ticketwww sh -c "cd $DATABASE_MIGRATION_SOURCE_DIR && git checkout -q - && git stash pop"
--- acceptance-amazon-rds-sync/sync-rds-db-instance.sh
+++ acceptance-amazon-rds-sync/sync-rds-db-instance.sh
@@ -75,14 +75,8 @@ mysql -h $DATABASE_HOST $DATABASE_NAME < $DIR/post-migration-tweaks.sql 1>/dev/n
 # Offset AUTOINCREMENT columns
 ./set-autoincrement.sh $DATABASE_NAME $DATABASE_HOST $DATABASE_OFFSET
 
-# Reset database migration files
-./reset-database-migration.sh $DATABASE_MIGRATION_SOURCE_DIR
-
-# Sync diff migration files
-./sync-migration.sh $DATABASE_MIGRATION_TARGET_DIR $DATABASE_MIGRATION_TARGET_DIR_DIFF $DATABASE_MIGRATION_SOURCE_DIR
-
-# Re-run previously executed database migration files
-./migrate-database.sh $DATABASE_MIGRATION_SOURCE_DIR
+# Execute database migration files
+./execute-migration.sh $DATABASE_MIGRATION_SOURCE_DIR
 
 # Restore user data in ts2acceptance database
 mysql -h $DATABASE_HOST $DATABASE_NAME <$DATABASE_USERDATA_SQL_FILE 1>/dev/null
